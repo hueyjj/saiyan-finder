aa4cf66070151c3b75e47935b9cd40f76b479b39
unknown <jasperjeng@gmail.com>
2019-05-15 05:32:09 -0700

    Spawn go routines for every directories and scan each file in them

--- main.go
+++ main.go
@@ -6,14 +6,16 @@ import (
 	"os"
 	"os/signal"
 	"path/filepath"
+	"sync"
 )
 
-var (
-	// CurrentWorkingDir is the current working directory
-	CurrentWorkingDir = ""
-)
+type commit struct {
+	sync.Mutex
+	files []string
+}
 
-type buf struct {
+type dirs struct {
+	sync.Mutex
 	queue []string
 }
 
@@ -32,38 +34,86 @@ func readDir(dirname string) ([]os.FileInfo, error) {
 }
 
 func main() {
-	CurrentWorkingDir, err := os.Getwd()
+	cwd, err := os.Getwd()
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("Current working directory:", CurrentWorkingDir)
+	// fmt.Println("Current working directory:", cwd)
 
-	buf := buf{}
+	addFilePath := make(chan string)
+	addDirPath := make(chan string)
+	done := make(chan bool)
 
-	newQueueItem := make(chan string)
+	commit := commit{}
+	dirs := dirs{}
+	dirs.queue = append(dirs.queue, cwd)
 
+	var wg sync.WaitGroup
 	go func() {
-		files, err := readDir(".")
-		if err != nil {
-			log.Fatal(err)
-		}
-		for _, file := range files {
-			path := filepath.FromSlash(fmt.Sprintf("%s/%s", CurrentWorkingDir, file.Name()))
-			fmt.Println(path)
-			newQueueItem <- path
+		for {
+			if len(dirs.queue) <= 0 {
+				done <- true
+			}
+			for i := 0; i < len(dirs.queue); i++ {
+				wg.Add(1)
+
+				var dir string
+				dir, dirs.queue = dirs.queue[0], dirs.queue[1:]
+				go func() {
+					defer wg.Done()
+					//var dir string
+					//dirs.Lock()
+					//if len(dirs.queue) > 0 {
+					//	dir, dirs.queue = dirs.queue[0], dirs.queue[1:]
+					//}
+					//dirs.Unlock()
+
+					//if len(dir) <= 0 {
+					//	return
+					//}
+
+					files, err := readDir(dir)
+					if err != nil {
+						log.Fatal(err)
+					}
+					fmt.Println("Working in:", dir)
+					for _, file := range files {
+						path := filepath.FromSlash(fmt.Sprintf("%s/%s", dir, file.Name()))
+						stat, err := os.Stat(path)
+						if err != nil {
+							fmt.Println(err)
+							continue
+						}
+						switch mode := stat.Mode(); {
+						case mode.IsDir():
+							addDirPath <- path
+						case mode.IsRegular():
+							addFilePath <- path
+						}
+					}
+				}()
+			}
+			wg.Wait()
 		}
 	}()
 
 	interrupt := make(chan os.Signal, 1)
 	signal.Notify(interrupt, os.Interrupt)
-
 	for {
 		select {
-		case item := <-newQueueItem:
-			buf.queue = append(buf.queue, item)
+		case dirPath := <-addDirPath:
+			dirs.queue = append(dirs.queue, dirPath)
+		case filePath := <-addFilePath:
+			commit.files = append(commit.files, filePath)
 		case <-interrupt:
-			fmt.Print(buf.queue)
-			fmt.Println("Ctrl+C detected. Ending program")
+			fmt.Println("Interrupt detected. Exiting program.")
+			fmt.Println("Number of files indexed:", len(commit.files))
+			os.Exit(1)
+			return
+		case <-done:
+			// fmt.Println("commit.files:", commit.files)
+			// fmt.Println("working.queue:", dirs.queue)
+			fmt.Println("Number of files indexed:", len(commit.files))
 			os.Exit(0)
 			return
 		}
